233c8e74e720a8bda549ff3a241db25de2101d55
Keigo Oka <ogiekako@gmail.com>
2020-03-18 22:13:48 +0900

    hoge2

--- main.go
+++ main.go
@@ -7,3 +7,8 @@ const DIR = ".git"
 func main() {
 	os.Open(DIR)
 }
+
+// object
+// - blob : only file contents.
+// - tree  : a directory contents (filename -> object hash)
+// - commit: commit object (tree hash + commit info)
